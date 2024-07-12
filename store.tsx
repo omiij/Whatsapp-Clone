4058c3bceb20429873acef2141cadfc7585cd04d
omiij <93545746+omiij@users.noreply.github.com>
2024-07-12 18:55:49 +0530

    Update store.tsx

--- store.tsx
+++ store.tsx
@@ -1,28 +1,52 @@
-import { connectRouter, routerMiddleware } from 'connected-react-router';
-import { createBrowserHistory } from 'history';
-import { applyMiddleware, createStore } from 'redux';
-import { persistReducer, persistStore } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import thunk from 'redux-thunk';
-import { middleWareDispatch } from './redux-store/middleware';
-import { rootReducer } from './redux-store/reducers/index';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { combineReducers } from 'redux';
+import applicationReducer, { ApplicationStateProps } from './application';
+import dashboardDoughnutReducer from './doughnutGraph';
+import monthOnMonthReducer from './monthsGraph';
+import topDistributorReducer from './topDistributors';
+import commitmentAndContributionReducer from './commitmentAndContributionGraph';
+import auth from './auth';
+import errorReducer, { ErrorState } from './error';
+import investorReducer from './investor';
+import profileReducer from './profile';
+import paramsReducer, { ParamsProps } from './queryParams';
+import toast, { ToastType } from './toast';
+import {
+  dashboardOnboardingType,
+  distributorWisecommitmentType,
+  monthwiseCommitmentAmountType,
+  monthwiseOnboardingSummaryType,
+} from '../types/api-types';
+export interface RootStateType {
+  auth: any;
+  router: any;
+  profileReducer: any;
+  dataDougnut: dashboardOnboardingType;
+  monthOnMonthData: monthwiseOnboardingSummaryType;
+  topDistributors: distributorWisecommitmentType;
+  commitmentAndContributionData: monthwiseCommitmentAmountType;
+  application: ApplicationStateProps;
+  toast: ToastType;
+  investor: any;
+  error: ErrorState;
+  paramsObj: ParamsProps;
+}
 
-export const history = createBrowserHistory();
+export const rootReducer = (history: any) =>
+  combineReducers({
+    router: history,
+    auth,
+    profile: profileReducer,
+    application: applicationReducer,
+    dataDougnut: dashboardDoughnutReducer,
+    monthOnMonthData: monthOnMonthReducer,
+    commitmentAndContributionData: commitmentAndContributionReducer,
+    topDistributors: topDistributorReducer,
+    toast,
+    investor: investorReducer,
+    error: errorReducer,
+    paramsObj: paramsReducer,
+  });
 
-const persistConfig = {
-  key: 'wa-user',
-  storage,
-  whitelist: ['auth'],
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer(connectRouter(history)));
-
-export const store = createStore(
-  persistedReducer,
-  applyMiddleware(thunk, middleWareDispatch, routerMiddleware(history))
-);
-// history.listen(() => {
-//     document.getElementById('db-content-right')?.scrollTo(0,0);
-// })
-export const persistor = persistStore(store);
-export default store;
+export type RootState = ReturnType<typeof rootReducer>;
